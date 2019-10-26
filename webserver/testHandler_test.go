e711317a333c662baf202d43abc33ef7781175f5
tsingsonqin <tsingson@gmail.com>
2019-10-27 00:18:09 +0800

    update unit testing , add benchmark

--- webserver/testHandler_test.go
+++ webserver/testHandler_test.go
@@ -14,7 +14,6 @@ import (
 
 func TestWebServer_simplePostHandler(t *testing.T) {
 	// setup logger that output to console
-
 	log := logger.Console()
 
 	// init a webServer to use the post handler method
@@ -27,18 +26,18 @@ func TestWebServer_simplePostHandler(t *testing.T) {
 	// setup listener , it's fasthttp in memory listener for TESTING only
 	ln := fasthttputil.NewInmemoryListener()
 
-	// remember to close listener
-	defer func() {
-		_ = ln.Close()
-	}()
-
 	// now running fasthttp server in a goroutine
-
+	handler := ws.simplePostHandler()
 	go func() {
-		err := fasthttp.Serve(ln, ws.simplePostHandler())
+		err := fasthttp.Serve(ln, handler)
 		if err != nil {
 			panic(fmt.Errorf("failed to serve: %v", err))
 		}
+	}()
+
+	// remember to close listener
+	defer func() {
+		_ = ln.Close()
 	}()
 
 	// -------------------------------------------------------
@@ -49,6 +48,7 @@ func TestWebServer_simplePostHandler(t *testing.T) {
 		Dial: func(addr string) (net.Conn, error) {
 			return ln.Dial()
 		},
+		DisableHeaderNamesNormalizing: true,
 	}
 
 	req := fasthttp.AcquireRequest()
@@ -75,4 +75,68 @@ func TestWebServer_simplePostHandler(t *testing.T) {
 
 	assert.Equal(t, resp.StatusCode(), 200)
 	assert.Equal(t, resp.Body(), payload)
+}
+
+func BenchmarkWebServer_simplePostHandler(b *testing.B) {
+	b.ReportAllocs()
+	// setup logger that output to console
+	log := logger.Console()
+
+	// init a webServer to use the post handler method
+	ws := &webServer{
+		Addr:  ":3000",
+		Log:   log,
+		debug: false,
+	}
+	handler := ws.simplePostHandler()
+
+	// setup listener , it's fasthttp in memory listener for TESTING only
+	ln := fasthttputil.NewInmemoryListener()
+
+	// now running fasthttp server in a goroutine
+
+	go func() {
+		err := fasthttp.Serve(ln, handler)
+		if err != nil {
+			panic(fmt.Errorf("failed to serve: %v", err))
+		}
+	}()
+
+	// remember to close listener
+	defer func() {
+		_ = ln.Close()
+	}()
+
+	// -------------------------------------------------------
+	// now, the real http client what you want
+	// -------------------------------------------------------
+
+	client := &fasthttp.Client{
+		Dial: func(addr string) (net.Conn, error) {
+			return ln.Dial()
+		},
+		DisableHeaderNamesNormalizing: true,
+	}
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer func() {
+		// 用完需要释放资源
+		fasthttp.ReleaseResponse(resp)
+		fasthttp.ReleaseRequest(req)
+	}()
+
+	postPayloadByte := []byte(`{"actual_start_date":"2019-07-29","actual_end_date":"2019-07-29","plan_start_date":"2019-07-29","plan_end_date":"2019-02-12","title":"养殖计划00002","user_id":2098735545843717147}`)
+
+	// req.SetRequestURI("http://localhost:3000/")
+	req.SetHost("localhost:3000")
+	req.Header.Add("Accept", "application/json")
+	req.Header.SetMethod("POST")
+
+	req.SetBody(postPayloadByte)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = client.Do(req, resp)
+	}
 }
